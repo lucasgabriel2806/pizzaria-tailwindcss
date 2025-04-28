de801e00e5385785178362dd1c16a1d568aeb748
Lucas Gabriel de Paula Pinto <102253585+lucasgabriel2806@users.noreply.github.com>
2025-04-28 19:48:28 +0000

    Adicinando mais funções(carrinho)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/** Elementos */
+/** Variáveis */
 const menu = document.getElementById("menu");
 const cartBtn = document.getElementById("cart-btn");
 const cartModal = document.getElementById("cart-modal");
@@ -9,6 +9,8 @@ const closeModalBtn = document.getElementById("close-modal-btn");
 const cartCounter = document.getElementById("cart-count");const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+let cart = [];
+
 // Abrir o modal do carrinho
 cartBtn.addEventListener("click", function() {
     cartModal.style.display = "flex";
@@ -16,5 +18,59 @@ cartBtn.addEventListener("click", function() {
 
 // Fechar o modal quando clicar fora
 cartModal.addEventListener("click", function(event) {
-    console.log(event);
+    // console.log(event);
+    if (event.target === cartModal) {
+        cartModal.style.display = "none"
+    }
 });
+
+closeModalBtn.addEventListener("click", function() {
+    cartModal.style.display = "none";
+});
+
+menu.addEventListener("click", function(event) {
+    // console.log(event.target);
+    let parentButton = event.target.closest(".add-to-cart-btn");
+
+    // console.log(parentButton);
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = pasreFloat(parentButton.getAttribute("data-price"));
+
+        // console.log(name, price);
+
+        // Adicionar no carrinho
+        addToCart(name, price);
+    }
+});
+
+// Função para adicionar no carrinho
+function addToCart(name, price){
+
+    const existingItem = cart.find(item => item.name === name);
+
+    if(existingItem){
+        // Se o item já existir no carrinho, aumente a quantidade
+        // console.log(existingItem);
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        });
+    }
+
+    updateCartModal();
+    
+}
+
+//Atualiza o carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+      // console.log(item);
+    })
+}
