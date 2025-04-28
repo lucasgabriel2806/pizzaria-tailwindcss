124fcbc499df20678b7a97734d5eb108c03a8a57
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-04-28 13:33:17 -0300

    Adicionando funções JavaScript

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ const closeModalBtn = document.getElementById("close-modal-btn");
 const cartCounter = document.getElementById("cart-count");const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+// Abrir o modal do carrinho
+cartBtn.addEventListener("click", function() {
+    cartModal.style.display = "flex";
+});
+
+// Fechar o modal quando clicar fora
+cartModal.addEventListener("click", function(event) {
+    console.log(event);
+});
