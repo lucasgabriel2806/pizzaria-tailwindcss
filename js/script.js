71f54fa01ccd2fc5734e38a3e9097ea176cd1153
Lucas Gabriel de Paula Pinto <102253585+lucasgabriel2806@users.noreply.github.com>
2025-04-29 20:04:58 +0000

    Atualizando função remover e adicionando funções de validação

--- js/script.js
+++ js/script.js
@@ -108,5 +108,73 @@ function updateCartModal(){
 
 // Função para remover item do carrinho
 cartItemsContainer.addEventListener("click", function(event) {
+   if(event.target.classList.contains("remove-from-cart-btn")) {
+       const name = event.target.getAttribute("data-name");
+       removeItemCart(name);
+
+       // console.log(name);
+   } 
+});
+
+funnction removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name))
+
+    if(index !== -1) {
+        const item = cart[index];
+        // console.log(item);
+
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500");
+        addressWarn.classList.add("hidden");
+    }
+});
+
+// Finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = checkRestaurantOpen();
+    if(isOpen){
+        
+    }
     
-})
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden");
+        addressInput.classList.add("border-red-500");
+        return;
+    }
+});
+
+// Verificar a hora e manipular o card horário
+function checkRestaurantOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22; 
+    // true = restaurante está aberto
+    
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if(isOpen) {
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+} else {
+    spanItem.classList.remove("bg-green-600");
+    spanItem.classList.add("bg-red-500");
+}
